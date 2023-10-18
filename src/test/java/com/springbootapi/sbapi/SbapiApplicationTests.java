fbe1ff3eb63a22546767b084960234207990dc85
Leandro Estevez <leoest.dev@gmail.com>
2023-10-18 15:15:12 -0500

    files

---
+++ src/test/java/com/springbootapi/sbapi/SbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootapi.sbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
